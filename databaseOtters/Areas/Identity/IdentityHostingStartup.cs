ebc80e77412e67e3f35db13d60fffa852ce10815
Adam Antoš <adam.antos@pslib.cz>
2020-10-13 16:33:04 +0200

    Commit 1

---
+++ databaseOtters/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using databaseOtters.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(databaseOtters.Areas.Identity.IdentityHostingStartup))]
+namespace databaseOtters.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<databaseOttersContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("databaseOttersContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<databaseOttersContext>();
+            });
+        }
+    }
+}
---
+++ databaseOtters/Model/Place.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace databaseOtters.Model
+{
+    public class Place
+    {
+        public string Name { get; set; }
+        public ICollection<Otter> Otters { get; set; }
+        [Required]
+        public Location Location { get; set; }
+
+        [ForeignKey("Location")]
+        public int LocationId { get; set; }
+    }
+}
